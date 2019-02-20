bde290f0dda23d1faa0b68e30a5f94eef17a5414
Christoph Kamil Wozniak <woezelmann@gmail.com>
2019-02-20 10:38:28 +0100

    Revert "v2 more changes"
    This reverts commit aacf6e89

--- src/com/woezelmann/showas/ScrollableLanguageTextField.java
+++ src/com/woezelmann/showas/ScrollableLanguageTextField.java
@@ -5,8 +5,6 @@ import com.intellij.openapi.editor.ex.EditorEx;
 import com.intellij.openapi.project.Project;
 import com.intellij.ui.LanguageTextField;
 
-import java.awt.*;
-
 public class ScrollableLanguageTextField extends LanguageTextField {
     public ScrollableLanguageTextField(Language language, Project project, String s) {
         super(language, project, s);
